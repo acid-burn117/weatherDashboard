e7311018e5cc5d6c9a2f3f0fe0066e8d50520567
acid-burn117 <osoriodouglas19@gmail.com>
2022-01-14 14:04:56 -0500

    api call

---
+++ scripts/scripts.js
@@ -0,0 +1,37 @@
+var input = document.getElementById("input")
+var searchbutton = document.getElementById("searchBttn")
+var cityWeather = document.getElementById("cityWeather")
+searchbutton.addEventListener("click", function(){
+    console.log(input.value)
+    APIcall(input.value)
+})
+function APIcall(city){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=b8033cd6f91ebcbedb1a8f2576574c01&units=imperial')
+  .then(response => response.json())
+  .then(data => 
+    {console.log(data)
+        cityWeather.innerHTML = ""
+    var forecastContainer = document.createElement("div")
+    var cityName = document.createElement("h1")
+    var temp = document.createElement("h3")
+    var wind = document.createElement("h3")
+    var humidity = document.createElement("h3")
+    var uvIndex = document.createElement("h3")
+    var icon= document.createElement("img")
+    cityName.innerText = data.name 
+    forecastContainer.append(cityName)
+    temp.innerText = data.main.temp
+    forecastContainer.append(temp)
+    wind.innerText = data.wind.speed
+    forecastContainer.append(wind)
+    humidity.innerText = data.main.humidity
+    forecastContainer.append(humidity)
+    icon.src="http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png"
+    forecastContainer.append(icon)
+    fetch()
+    cityWeather.append(forecastContainer)
+    });
+    
+}
+
+
